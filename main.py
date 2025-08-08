7856133b7b0ab5989f491641e5d814b07b2617fe
onebadduc <conjsp@gmail.com>
2025-08-07 20:15:39 -0700

    finished bookbot and added cli commands

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import word_count
 from stats import character_count
 from stats import sorted_list
+import sys
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
@@ -9,21 +10,25 @@ def get_book_text(file_path):
 def main():
     char_count = {}
     sort_char = []
-    file_path = "books/frankenstein.txt"
-    file_contents = get_book_text(file_path)
-    total_words = word_count(file_contents)
-    char_count = character_count(file_contents)
-    sort_char = sorted_list(char_count)
-    char_count = dict(sort_char)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {file_path}...")
-    print("----------- Word Count -----------")
-    print(f"Found {total_words} total words")
-    print("--------- Character Count ---------")
-    for key, value in char_count.items():
-        if key.isalpha():
-            print(f"{key}: {value}")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+        file_contents = get_book_text(file_path)
+        total_words = word_count(file_contents)
+        char_count = character_count(file_contents)
+        sort_char = sorted_list(char_count)
+        char_count = dict(sort_char)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file_path}...")
+        print("----------- Word Count -----------")
+        print(f"Found {total_words} total words")
+        print("--------- Character Count ---------")
+        for key, value in char_count.items():
+            if key.isalpha():
+                print(f"{key}: {value}")
         
-    print("=========== END ===========")
+        print("=========== END ===========")
 
 main()
